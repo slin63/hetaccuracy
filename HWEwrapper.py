2e0b75b3a16f38e0de8dc0d8e87279fd593e376f
Shean Lin <slin63@illinois.edu>
2016-11-01 14:58:43 -0500

    Add default values to hwewrapper.py

--- HWEwrapper.py
+++ HWEwrapper.py
@@ -1,30 +1,32 @@
 import argparse
 import os
 
+# Default values
 HWEFILTER_NAME = "bash HWEfiltering_phetexcessfilter.sh"
+KEEP = "keep_seq.txt"
+PVAL = 0.001
+GATK = "final.GATK.vcf.recode.vcf"
+GATKGZ = "final.GATK.vcf.recode.vcf.gz"
 
 parser = argparse.ArgumentParser(description='Calls HWEfilter with the following arguments')
-parser.add_argument('-KEEP', help='Name of the keep file')
-parser.add_argument('-GATK', help='Name of the GATK file')
-parser.add_argument('-GATKGZ', help='Name of the GATKGZ file')
-parser.add_argument('-PVAL', type=float, help='Pval to use for cutoff determination')
+parser.add_argument('-KEEP', default=KEEP, help='Name of the keep file')
+parser.add_argument('-GATK', default=GATK, help='Name of the GATK file')
+parser.add_argument('-GATKGZ', default=GATKGZ, help='Name of the GATKGZ file')
+parser.add_argument('-PVAL', default=PVAL, type=float, help='Pval to use for cutoff determination')
 
 args = parser.parse_args()
 
-if None in args.__dict__.values():
-    raise KeyError("Not enough arguments")
-else:
-    os.system("{} {} {} {} {}".format(
-        HWEFILTER_NAME,
-        args.KEEP,
-        args.GATK,
-        args.GATKGZ,
-        args.PVAL
-    ))
+import pdb; pdb.set_trace()  # breakpoint f497dd90 //
+
+
+os.system("{} {} {} {} {}".format(
+    HWEFILTER_NAME,
+    args.KEEP,
+    args.GATK,
+    args.GATKGZ,
+    args.PVAL
+))
 
 
 
-# KEEP=keep_seq.txt
-# PVAL=0.001
-# GATK=final.GATK.vcf.recode.vcf
-# GATKGZ=final.GATK.vcf.recode.vcf.gz
+
